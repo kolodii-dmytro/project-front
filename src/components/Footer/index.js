e00e8f3c5fef92bdd6efaab0d81efe6309facdea
dmytro <kolodii.dmytro@gmail.com>
2019-09-03 11:45:20 +0300

    test 3

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
     render() {
         return (
             <footer>
-            footer
+            footer is changed 
           </footer>
         )
     }
