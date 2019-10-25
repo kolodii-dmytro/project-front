170546f9eb183f7a292fd3e1e13dfb503145bf19
dmytro <kolodii.dmytro@gmail.com>
2019-10-25 17:05:27 +0300

    wip

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from "react"; 
 import {  Route } from "react-router-dom";
 
 function Index() {
-    return <h2>Home</h2>;
+    return <h2>Home2</h2>;
   }
   
   function About() {
--- src/components/Nav/NavItem.js
+++ src/components/Nav/NavItem.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 
+
 export default  function NavItem({item, text}) {
+
     return (
     <li>
-        <Link to={item}>{text}</Link>
+        <Link to={item}>{'res2'+text}</Link>
     </li>
     )
 }
